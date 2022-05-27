ae52e9ba2a48d6cbbbe13bfd6081b3922133f1c3
Capseeyool <capsulechew@gmail.com>
2022-05-28 03:36:24 +0800

    Complete basic functionality

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Osu! Practice Tool
 
 OPT is a discord bot meant for tournament players to practice certain map picks.
+
+## Usage
+`.m {mod}`: Randomly selects a map according to the mod
+Example: `.m NM1`
+
+More commands WIP
+
+## TODO
+- `main.py` 24-26
+- Dockerize app
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 import requests
 import sqlite3
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,6 +11,8 @@ con = sqlite3.connect('db.db')
 cur = con.cursor()
 
 API_KEY = os.getenv('API_KEY')
+TOKEN = os.getenv('TOKEN')
+
 PREFIX = '.'
 
 class OPT(discord.Client):
@@ -18,17 +21,22 @@ class OPT(discord.Client):
     
     async def on_message(self, message):
         msg = message.content.split(' ')
+        # TODO
+        # list all tournaments instead of only 1 per map
+        # calculate cs/ar/od changes
         if msg[0] == f'{PREFIX}m':
             m = random.choice(cur.execute(f'SELECT * FROM db WHERE mod=\'{msg[1].upper()}\'').fetchall())
-            r = requests.get(f'https://osu.ppy.sh/api/get_beatmaps?k={API_KEY}&b={m[0]}&m=0').json()[0]
+            r = requests.get(f'https://osu.ppy.sh/api/get_beatmaps?k={API_KEY}&b={m[2]}&m=0').json()[0]
             embed = discord.Embed(
-                title=f'{r["title"]} [{r["version"]}]',
-                description=f'{m[2]}: {m[1]}\nID: {m[0]}',
-                url=f'https://osu.ppy.sh/beatmapsets/{r["beatmapset_id"]}#osu/{m[0]}',
+                title=f'{r["artist"]} - {r["title"]} [{r["version"]}]',
+                description=f'{m[0]} | {m[1]} | {m[2]}\n'
+                f'**SR**: {round(float(r["difficultyrating"]), 2)} | **Length**: {int(r["total_length"]) // 60}:{int(r["total_length"]) % 60} | **Combo**: {r["max_combo"]}x\n'
+                f'**BPM**: {r["bpm"]} | **CS**: {r["diff_size"]} | **AR**: {r["diff_approach"]} | **OD**: {r["diff_overall"]}',
+                url=f'https://osu.ppy.sh/beatmapsets/{r["beatmapset_id"]}#osu/{m[2]}',
             )
             embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{r["beatmapset_id"]}/covers/cover.jpg')
             await message.channel.send(embed=embed)
 
 if __name__ == '__main__':
     client = OPT()
-    client.run(os.getenv('token'))
+    client.run(TOKEN)
