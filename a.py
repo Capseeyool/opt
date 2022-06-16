bfe6eb3db24e299c7a686ffa7e02f079cefaeb62
Capseeyool <capsulechew@gmail.com>
2022-06-17 05:23:20 +0800

    Add SR filtering

--- a.py
+++
@@ -1,8 +0,0 @@
-import random
-import sqlite3
-
-con = sqlite3.connect('db.db')
-cur = con.cursor()
-
-m = random.choice(cur.execute(f'SELECT * FROM db WHERE mod=\'NM1\'').fetchall())
-print(all(m))
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import random
 import requests
 import sqlite3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 con = sqlite3.connect('db.db')
 cur = con.cursor()
@@ -21,53 +24,31 @@ class OPT(discord.Client):
         # TODO
         # list all tournaments instead of only 1 per map
         # calculate cs/ar/od changes
+        # use query parameters instead
         if msg[0] == f'{PREFIX}m':
-            m = random.choice(cur.execute(f'SELECT * FROM db WHERE mod=\'{msg[1].upper()}\'').fetchall())
-            if (all(m)):
-                embed = discord.Embed(
-                    title=f'{m[3]} - {m[4]} [{m[5]}]',
-                    description=f'{m[0]} | {m[1]} | {m[2]}\n'
-                    f'**SR**: {round(float(m[6]), 2)} | **Length**: {int(m[7]) // 60}:{int(m[7]) % 60} | **Combo**: {m[8]}x\n'
-                    f'**BPM**: {m[9]} | **CS**: {m[10]} | **AR**: {m[11]} | **OD**: {m[12]}',
-                    url=f'https://osu.ppy.sh/beatmapsets/{m[13]}#osu/{m[2]}',
-                )
+            if len(msg) in range(2, 4):
+                if len(msg) == 2:
+                    m = random.choice(cur.execute(f'SELECT * FROM db WHERE mod=\'{msg[1].upper()}\'').fetchall())
+                elif len(msg) == 3:
+                    try:
+                        m = random.choice(cur.execute(f'SELECT * FROM db WHERE mod=\'{msg[1].upper()}\' AND SR >= {msg[2]} AND SR <= {int(msg[2]) + 1}').fetchall())
+                    except IndexError:
+                        m = []
+                try:
+                    embed = discord.Embed(
+                        title=f'{m[3]} - {m[4]} [{m[5]}]',
+                        description=f'{m[0]} | {m[1]} | {m[2]}\n'
+                        f'**SR**: {round(float(m[6]), 2)} | **Length**: {int(m[7]) // 60}:{int(m[7]) % 60} | **Combo**: {m[8]}x\n'
+                        f'**BPM**: {m[9]} | **CS**: {m[10]} | **AR**: {m[11]} | **OD**: {m[12]}',
+                        url=f'https://osu.ppy.sh/beatmapsets/{m[13]}#osu/{m[2]}',
+                    )
+                    embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{m[13]}/covers/cover.jpg')
+                    await message.channel.send(embed=embed)
+                except IndexError:
+                    await message.channel.send('404 not found')
             else:
-                r = requests.get(f'https://osu.ppy.sh/api/get_beatmaps?k={API_KEY}&b={m[2]}&m=0').json()[0]
-                # cur.execute(f'''INSERT INTO db (artist, title, diff, SR, length, combo, BPM, CS, AR, OD, beatmapsetID) VALUES (
-                #     \'{r["artist"]}\',
-                #     \'{r["title"]}\',
-                #     \'{r["version"]}\',
-                #     \'{r["difficultyrating"]}\',
-                #     \'{r["total_length"]}\',
-                #     \'{r["max_combo"]}\',
-                #     \'{r["bpm"]}\',
-                #     \'{r["diff_size"]}\',
-                #     \'{r["diff_approach"]}\',
-                #     \'{r["diff_overall"]}\',
-                #     \'{r["beatmapset_id"]}\'
-                # )''')
-                cur.execute(f'''UPDATE db SET
-                    artist = \'{r["artist"]}\',
-                    title = \'{r["title"]}\',
-                    diff = \'{r["version"]}\',
-                    SR = {r["difficultyrating"]},
-                    length = {r["total_length"]},
-                    combo = {r["max_combo"]},
-                    BPM = {r["bpm"]},
-                    CS = {r["diff_size"]},
-                    AR = {r["diff_approach"]},
-                    OD = {r["diff_overall"]},
-                    beatmapsetID = {r["beatmapset_id"]}
-                WHERE tournament = {m[0]} AND mod = {m[1]}''')
-                embed = discord.Embed(
-                    title=f'{r["artist"]} - {r["title"]} [{r["version"]}]',
-                    description=f'{m[0]} | {m[1]} | {m[2]}\n'
-                    f'**SR**: {round(float(r["difficultyrating"]), 2)} | **Length**: {int(r["total_length"]) // 60}:{int(r["total_length"]) % 60} | **Combo**: {r["max_combo"]}x\n'
-                    f'**BPM**: {r["bpm"]} | **CS**: {r["diff_size"]} | **AR**: {r["diff_approach"]} | **OD**: {r["diff_overall"]}',
-                    url=f'https://osu.ppy.sh/beatmapsets/{r["beatmapset_id"]}#osu/{m[2]}',
-                )
-                embed.set_image(url=f'https://assets.ppy.sh/beatmaps/{r["beatmapset_id"]}/covers/cover.jpg')
-            await message.channel.send(embed=embed)
+                await message.channel.send('?')
+
         
         elif msg[0] == f'{PREFIX}mappool':
             pool = cur.execute(f'SELECT mod, ID FROM db WHERE tournament=\'{message.content[9:]}\'').fetchall()
